11b2a97751ec0f77b3e3a3178ffbef88e63c5338
Varshil-Shah <varshilashah09@gmail.com>
2021-12-25 11:13:28 +0530

    :art: Define function to get live stock and name

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import bs4.element
 names = ["IRCTC.NS"]
 url = "https://finance.yahoo.com/quote/{0}?p={0}&.tsrc=fin-srch"
 
-source = requests.get(url.format(names[0])).text
-soup = BeautifulSoup(source, "lxml")
 
-current_stock_data = soup.find("div", class_="My(6px) Pos(r) smartphone_Mt(6px) W(100%)")
-print(current_stock_data)
+def get_name_of_stock(element: bs4.element):
+    prev_element = element.previous_sibling
+    company_name = prev_element.find("h1").text
+    print(company_name)
+
+
+def get_live_stock(tickers: list[str]):
+    for ticker in tickers:
+        source = requests.get(url.format(ticker)).text
+        soup = BeautifulSoup(source, "lxml")
+
+        current_stock_data = soup.find("div", class_="My(6px) Pos(r) smartphone_Mt(6px) W(100%)")
+        get_name_of_stock(current_stock_data)
+        print(current_stock_data)
+
+
+if __name__ == '__main__':
+    get_live_stock(names)
